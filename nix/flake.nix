09efa2dfb196eec8fc468151ae07344e2e8d6d7f
trillo <trillo@trillos-MacBook-Pro.local>
2025-04-28 13:55:59 -0600

    initial commit

---
+++ nix/flake.nix
@@ -0,0 +1,162 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-core
+    , homebrew-cask, }:
+    let
+      configuration = { pkgs, ... }: {
+        nixpkgs.config.allowUnfree = true;
+        # $ nix-env -qaP | grep wget
+        environment.systemPackages = with pkgs; [
+          aerospace
+          atuin
+          tree
+          awscli2
+          cargo
+          chatgpt
+          coreutils
+          curl
+          fd
+          fzf
+          git
+          keycastr
+          kitty
+          lazygit
+          moreutils
+          neovim
+          nixfmt-classic
+          obsidian
+          ripgrep
+          slack
+          stow
+          telegram-desktop
+          vscode
+          wget
+          just
+        ];
+        environment.variables.EDITOR = "nvim";
+
+        homebrew = {
+          onActivation = {
+            # Fetch the newest stable branch of Homebrew's git repo
+            autoUpdate = true;
+            # Upgrade outdated casks, formulae, and App Store apps
+            upgrade = true;
+            # 'zap': uninstalls all formulae(and related files) not listed in the generated Brewfile
+            cleanup = "zap";
+          };
+          enable = true;
+          brews = [
+
+            {
+              name = "FelixKratz/formulae/sketchybar";
+              restart_service = true;
+            }
+          ];
+          casks = [
+            "raycast"
+            "firefox"
+            "1password"
+            "discord"
+            "docker"
+            "figma"
+            "google-chrome"
+            "krisp"
+            "spotify"
+            "uhk-agent"
+            "polypane"
+            "webstorm"
+          ];
+        };
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        programs.zsh.enable = true;
+
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 6;
+
+        nixpkgs.hostPlatform = "aarch64-darwin";
+        security.pam.services.sudo_local.touchIdAuth = true;
+        fonts.packages = with pkgs; [
+          nerd-fonts.fira-code
+          nerd-fonts.droid-sans-mono
+        ];
+        system.defaults = {
+          loginwindow = {
+            GuestEnabled = false; # disable guest user
+            SHOWFULLNAME = true; # show full name in login window
+          };
+          menuExtraClock.Show24Hour = true;
+          dock = {
+            autohide = true;
+            orientation = "right";
+            persistent-apps = [ ];
+            show-recents = false; # disable recent apps
+          };
+          finder = {
+            _FXShowPosixPathInTitle = true;
+            AppleShowAllExtensions = true;
+            FXEnableExtensionChangeWarning = false;
+            QuitMenuItem = true;
+            ShowPathbar = true;
+            ShowStatusBar = true;
+          };
+          NSGlobalDomain = {
+            "com.apple.swipescrolldirection" = true;
+            "com.apple.sound.beep.feedback" = 0;
+            AppleInterfaceStyle = "Dark";
+            AppleKeyboardUIMode = 3;
+            _HIHideMenuBar = true;
+            ApplePressAndHoldEnabled = true;
+            InitialKeyRepeat = 15;
+            KeyRepeat = 1;
+            NSAutomaticCapitalizationEnabled = false;
+            NSAutomaticDashSubstitutionEnabled = false;
+            NSAutomaticPeriodSubstitutionEnabled = false;
+            NSAutomaticQuoteSubstitutionEnabled = false;
+            NSAutomaticSpellingCorrectionEnabled = false;
+            NSNavPanelExpandedStateForSaveMode = true;
+            NSNavPanelExpandedStateForSaveMode2 = true;
+          };
+        };
+      };
+    in {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#simple
+      darwinConfigurations."mini" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              enable = true;
+              enableRosetta = true;
+              user = "trillo";
+              autoMigrate = true;
+            };
+          }
+        ];
+      };
+    };
+}
