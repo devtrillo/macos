f07a61d6b019e8ce9a2abf8551287ee4f36c1199
Esteban Trillo <esteban@devtrillo.com>
2025-04-28 17:37:37 -0600

    sketckybar

--- aerospace/.config/sketchybar/plugins/cpu.sh
+++ aerospace/.config/sketchybar/plugins/cpu.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 CORE_COUNT=$(sysctl -n machdep.cpu.thread_count)
 CPU_INFO=$(ps -eo pcpu,user)
 CPU_SYS=$(echo "$CPU_INFO" | grep -v $(whoami) | sed "s/[^ 0-9\.]//g" | awk "{sum+=\$1} END {print sum/(100.0 * $CORE_COUNT)}")
