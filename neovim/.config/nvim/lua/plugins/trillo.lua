c620b6a16abe1a8fd241e9a5a52d2929f391a6f5
Esteban Trillo <esteban@devtrillo.com>
2025-12-18 15:17:25 -0600

    add new nvim version

--- neovim/.config/nvim/lua/plugins/trillo.lua
+++ neovim/.config/nvim/lua/plugins/trillo.lua
@@ -26,7 +26,7 @@ return {
       { "-", "<cmd>Oil<cr>" },
     },
     -- Optional dependencies
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   },
