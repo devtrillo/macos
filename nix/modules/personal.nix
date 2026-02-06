d75d04918cc2eb81569eded86dda31d0d13482b2
Esteban Trillo <esteban@devtrillo.com>
2026-02-06 09:30:43 -0600

    modify personal config

--- nix/modules/personal.nix
+++ nix/modules/personal.nix
@@ -1,10 +1,34 @@
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ kitty ];
+{ pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    gnupg
+    jdk
+    kitty
+    pass
+    pinentry_mac
+  ];
 
   homebrew = {
     taps = [ ];
-    masApps = { "1Password for Safari" = 1569813296; };
-    brews = [ ];
-    casks = [ "plex-media-server" "tailscale" ];
+    masApps = {
+      "1Password for Safari" = 1569813296;
+    };
+    brews = [
+      "awscli"
+      "opencode"
+      "luarocks"
+    ];
+    casks = [
+      "arc"
+      "cloudflare-warp"
+      "karabiner-elements"
+      "plex-media-server"
+      "pycharm"
+      "slack"
+      "tableplus"
+      "tailscale"
+      "webstorm"
+      "whatsapp"
+    ];
   };
 }
